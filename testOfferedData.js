2891646e0d863a7cf0cf9aff62a42287974c3a9f
DevAliRaza <syedalirazarizvi60@gmail.com>
2025-04-28 13:53:02 +0500

    data file

---
+++ testOfferedData.js
@@ -0,0 +1,22 @@
+export const servicesOfffered=[{
+    id:1,
+    name:'Blood Test',
+    price:1000,
+    description:'Blood test for various diseases',
+   
+  },
+  {
+    id:2,
+    name:'Urine Test',
+    price:500,
+    description:'Urine test for various diseases',
+   
+  },
+  {
+    id:3,
+    name:'X-Ray',
+    price:2000,
+    description:'X-Ray for various body parts',
+   
+  }]
+
